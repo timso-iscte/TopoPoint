f875a9b65b7298440ee26f372557d5a4bd7c87c7
timso-iscte <timso@iscte-iul.pt>
2023-03-10 00:16:35 +0000

    changed variable names on scripts to more explicit names. Changed ps1 script to write the output to a file

--- TopoScriptLinux.sh
+++ TopoScriptLinux.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-File="dados.txt"
-FinalFile="altitude.txt"
+File="data.txt"
+ExportFile="elevation.txt"
 
-[ -e $FinalFile ] && rm -v $FinalFile
+[ -e $ExportFile ] && rm -v $ExportFile
 
 while IFS=" " read -r step line
 do
-	echo "$step  $(curl -s $line | grep "elevation" | sed 's/[^0-9.]//g' )" | tee -a $FinalFile
+	echo "$step  $(curl -s $line | grep "elevation" | sed 's/[^0-9.]//g' )" | tee -a $ExportFile
   sleep 1
 done < $File
--- TopoScriptWindows.ps1
+++ TopoScriptWindows.ps1
@@ -1,9 +1,13 @@
-$FilePath = Join-Path -Path $pwd -ChildPath "dados.txt"
-$FILE = Get-Content $FilePath
+$ImportPath = Join-Path -Path $pwd -ChildPath "data.txt"
+$ExportPath = Join-Path -Path $pwd -ChildPath "elevation.txt"
+$FILE = Get-Content $ImportPath
+
+If (Test-Path $ExportPath) {
+    Remove-Item $ExportPath
+}
+
 foreach ($LINE in $FILE){
 	$Response = Invoke-WebRequest -URI $LINE.Split(' ')[1]
 	$ParseResponse = $Response.Content | Out-String | ConvertFrom-Json
-	
-	Write-Output ($LINE.Split(' ')[0] + " " + $ParseResponse.results.elevation)
-
+	($LINE.Split(' ')[0] + " " + $ParseResponse.results.elevation) >> $pwd\elevation.txt
 }
